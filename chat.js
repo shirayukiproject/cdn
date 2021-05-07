bcd2278d3c81b4643e331abdcf5c685bdd7ed1ec
Kazuki278 <34799053+niskala5570@users.noreply.github.com>
2021-05-07 09:52:08 +0800

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Arleth98</title>
+</head>
+<style>
+body{margin:0;width: 97%;}
+</style>
+<body>
+<script id="cid0020000152421412160" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"arleth-desu","arch":"js","styles":{"a":"222222","b":100,"c":"FFFFFF","d":"FFFFFF","k":"222222","l":"222222","m":"222222","n":"FFFFFF","p":"10","q":"222222","r":100}}</script>
+</body>
+</html
