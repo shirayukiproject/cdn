1047b5d39403040e92beff140b272e0e8e74e687
Kazuki278 <34799053+niskala5570@users.noreply.github.com>
2021-05-07 09:53:33 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,11 +1,11 @@
 <html>
 <head>
-<title>Arleth98</title>
+<title>Shirayuki</title>
 </head>
 <style>
 body{margin:0;width: 97%;}
 </style>
 <body>
-<script id="cid0020000152421412160" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"arleth-desu","arch":"js","styles":{"a":"222222","b":100,"c":"FFFFFF","d":"FFFFFF","k":"222222","l":"222222","m":"222222","n":"FFFFFF","p":"10","q":"222222","r":100}}</script>
+<script id="cid0020000281343523049" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"shirayukiproject","arch":"js","styles":{"a":"4595dc","b":100,"c":"000000","d":"000000","k":"4595dc","l":"4595dc","m":"4595dc","p":"10","q":"4595dc","r":100,"cnrs":"0.35","fwtickm":1}}</script>
 </body>
 </html
